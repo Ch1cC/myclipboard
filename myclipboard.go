fffaf9ef5373fd89562ab779dafd6449e7303808
Ch1cC Chen <48575759+Ch1cC@users.noreply.github.com>
2023-02-18 20:29:57 +0800

    add:duration param

--- myclipboard.go
+++ myclipboard.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,7 +10,10 @@ import (
 	"time"
 )
 
-var kV sync.Map
+var (
+	kV       sync.Map
+	duration time.Duration
+)
 
 // Set 缓存过期功能实现 类Redis
 func Set(key interface{}, value interface{}, exp time.Duration) {
@@ -21,7 +25,7 @@ func Set(key interface{}, value interface{}, exp time.Duration) {
 
 func postData(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	Set(time.Now().UnixNano(), r.Form.Get("data"), time.Minute*5)
+	Set(time.Now().UnixNano(), r.Form.Get("data"), duration)
 	w.Header().Set("content-type", "text/json")
 	jsonobj := make(map[int64]interface{})
 	kV.Range(func(k interface{}, v interface{}) bool {
@@ -29,7 +33,8 @@ func postData(w http.ResponseWriter, r *http.Request) {
 		return true
 	})
 	msg, _ := json.Marshal(jsonobj)
-	fmt.Fprintf(w, string(msg)) // 这个写入到 w 的是输出到客户端的
+	result := string(msg)
+	fmt.Fprintf(w, result) // 这个写入到 w 的是输出到客户端的
 }
 func getData(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "text/json")
@@ -43,10 +48,13 @@ func getData(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	flag.DurationVar(&duration, "duration", time.Minute*15, "过期时间间隔,默认15分钟")
 	http.HandleFunc("/post", postData) // 设置访问的路由
 	http.HandleFunc("/get", getData)   // 设置访问的路由
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	fmt.Println("启动在127.0.0.1:9090")
+	flag.Parse()
+	fmt.Printf("过期时间间隔设置为%s\n", duration)
 	err := http.ListenAndServe("127.0.0.1:9090", nil) // 设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
