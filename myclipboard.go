003c012cbcba21fed5dfe457f7fe4b191cade633
Ch1cC Chen <48575759+Ch1cC@users.noreply.github.com>
2023-02-26 17:11:57 +0800

    add:广播模式

--- myclipboard.go
+++ myclipboard.go
@@ -12,34 +12,20 @@ import (
 	"time"
 )
 
-// Put 缓存过期功能实现 类Redis
-
-/*func postData(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	unixMicro := time.Now().UnixMicro()
-	convert.Put(unixMicro, clipboard.Clipboard{UnixMicro: unixMicro, Msg: r.Form.Get("data")}, config.Duration)
-	w.Header().Set("content-type", "text/json")
-	msg, _ := json.Marshal(convert.BuildJson())
-	fmt.Fprintf(w, string(msg)) // 这个写入到 w 的是输出到客户端的
-}
-func getData(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "text/json")
-	msg, _ := json.Marshal(convert.BuildJson())
-	fmt.Fprintf(w, string(msg)) // 这个写入到 w 的是输出到客户端的
-}*/
-
 func main() {
 	flag.DurationVar(&config.Duration, "duration", time.Minute*15, "过期时间间隔,默认15分钟")
-	/*http.HandleFunc("/post", postData)       // 设置访问的路由
-	http.HandleFunc("/get", getData)         // 设置访问的路由*/
-	http.HandleFunc("/ws", ws.SocketHandler) // 设置访问的路由
+	hub := ws.NewHub()
+	go hub.Run()
+	http.HandleFunc("/ws", func(rw http.ResponseWriter, r *http.Request) {
+		ws.ServeWs(hub, rw, r)
+	}) // 设置访问的路由
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	fmt.Println("启动在127.0.0.1:9090")
 	flag.Parse()
 	fmt.Printf("过期时间间隔设置为%s\n", config.Duration)
 	convert.KV.Store(time.Now().UnixMicro(), clipboard.Clipboard{UnixMicro: time.Now().UnixMicro(), Msg: "欢迎使用"})
 	convert.KV.Store(time.Now().UnixMicro()+1, clipboard.Clipboard{UnixMicro: time.Now().UnixMicro(), Msg: "过期时间间隔为" + config.Duration.String()})
-	err := http.ListenAndServe("127.0.0.1:9090", nil) // 设置监听的端口
+	err := http.ListenAndServe("0.0.0.0:9090", nil) // 设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
