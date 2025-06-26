28eba65535c31ccb4498fd84459ec29141d5efa9
Ch1cC <48575759+Ch1cC@users.noreply.github.com>
2025-06-27 01:54:54 +0800

    fix: 重命名主程序

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"myclipboard/config"
+	"myclipboard/convert"
+	"myclipboard/logx"
+	"myclipboard/ws"
+	"net/http"
+	"time"
+)
+
+var version string // 用于存储版本号
+var port int
+
+func main() {
+	flag.DurationVar(&config.Duration, "duration", time.Minute*15, "过期时间间隔")
+	flag.IntVar(&port, "port", 9090, "端口")
+	hub := ws.NewHub()
+	go hub.Run()
+	http.HandleFunc("/websocket", func(w http.ResponseWriter, r *http.Request) {
+		ws.ServeWs(hub, w, r)
+	})
+	// 设置访问的路由
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	http.ServeFile(w, r, "./html/dist/index.html")
+	// })
+	// http.Handle("/dist/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Add("Cache-Control", "no-cache")
+	// 	if strings.HasSuffix(r.URL.Path, ".wasm") {
+	// 		w.Header().Set("content-type", "application/wasm")
+	// 	}
+	// 	http.StripPrefix("/dist/", http.FileServer(http.Dir("./html/dist"))).ServeHTTP(w, r)
+	// }))
+	flag.Parse()
+	logx.Logger.Printf(":%d\n", port)
+	logx.Logger.Printf("过期时间间隔设置为%s\n", config.Duration)
+	logx.Logger.Printf("当前版本号%s\n", version)
+	convert.KV.Store(time.Now().Unix(), convert.Row{Unix: time.Now().Unix(), Msg: []byte("当前版本号:" + version)})
+	convert.KV.Store(time.Now().Unix()+1, convert.Row{Unix: time.Now().Unix(), Msg: []byte("过期时间间隔为:" + config.Duration.String())})
+	err := http.ListenAndServeTLS(fmt.Sprintf(":%d", port), nil) // 设置监听的端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
