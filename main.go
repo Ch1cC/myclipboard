86e5b51fb3723182299894ea30c8ccd3164bdf2d
Ch1cC <48575759+Ch1cC@users.noreply.github.com>
2025-06-27 01:57:41 +0800

    fix: 移除旧的 myclipboard.go 文件

--- main.go
+++ main.go
@@ -14,10 +14,14 @@ import (
 
 var version string // 用于存储版本号
 var port int
+var crtPath string
+var keyPath string
 
 func main() {
 	flag.DurationVar(&config.Duration, "duration", time.Minute*15, "过期时间间隔")
 	flag.IntVar(&port, "port", 9090, "端口")
+	flag.StringVar(&crtPath, "crtPath", "./server.crt", "证书文件")
+	flag.StringVar(&keyPath, "keyPath", "./server.key", "证书密钥")
 	hub := ws.NewHub()
 	go hub.Run()
 	http.HandleFunc("/websocket", func(w http.ResponseWriter, r *http.Request) {
@@ -40,7 +44,7 @@ func main() {
 	logx.Logger.Printf("当前版本号%s\n", version)
 	convert.KV.Store(time.Now().Unix(), convert.Row{Unix: time.Now().Unix(), Msg: []byte("当前版本号:" + version)})
 	convert.KV.Store(time.Now().Unix()+1, convert.Row{Unix: time.Now().Unix(), Msg: []byte("过期时间间隔为:" + config.Duration.String())})
-	err := http.ListenAndServeTLS(fmt.Sprintf(":%d", port), nil) // 设置监听的端口
+	err := http.ListenAndServeTLS(fmt.Sprintf(":%d", port), crtPath, keyPath, nil) // 设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
